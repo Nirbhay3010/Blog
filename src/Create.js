b5451a6e568f103fc10b7aef60f37648f584d25f
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-07-03 00:11:48 +0530

    Added Post Request to update data

--- src/Create.js
+++ src/Create.js
@@ -5,12 +5,27 @@ function Create() {
     const [title, settitle] = useState('');
     const [body, setbody] = useState('');
     const [author, setauthor] = useState('mario');
+    const [isPending,setisPending]=useState(false);
 
+    const handleSubmit = (e) =>
+    {
+        setisPending(true)
+        e.preventDefault();
+        const blog={title,body,author};
+        fetch('http://localhost:8000/blogs',{
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            setisPending(false)
+        }
+        )
+    }
     return (
         <div className="create">
         <div>
             <h1>Add A Blog</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Blog title:</label>
             <input type="text"
             required
@@ -26,7 +41,9 @@ function Create() {
                 <option value="mario">mario</option>
                 <option value="yoshi">yoshi</option>
             </select>
-            <button>Add Blog</button>
+            {!isPending && <button type="submit">Add Blog</button>}
+            {isPending && <button disabled>Adding Blog</button>}
+
         </form>
         </div>
         </div>
